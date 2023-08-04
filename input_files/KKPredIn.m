06cdac072b4a0bc93a0b95613550dd7945006dd2
davlo199 <108636589+davlo199@users.noreply.github.com>
2023-08-04 14:19:11 +1200

    Add files via upload

---
+++ input_files/KKPredIn.m
@@ -0,0 +1,11 @@
+dataname="KaikoraQuakes";
+A=1;
+B=2398;
+M0=2.75;
+alp=0.329;
+m=2.209;
+gam=1.244;
+bet=0.72;
+c=0.567;
+PAR=1;
+parallel.slurmParameters={'--mem-per-cpu', '200m', '--time', '00:35:00','--cpus-per-task','12'};
